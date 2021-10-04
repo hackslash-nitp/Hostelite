583ad1beb5a3e7a666b169924ab0aa2009c74926
Srijan Shovit <srijanshovit@gmail.com>
2021-10-04 11:04:11 +0530

    map created in firestore but with overwriting problem

--- lib/complaintStudent.dart
+++ lib/complaintStudent.dart
@@ -7,20 +7,18 @@ import 'package:flutter/material.dart';
 import 'package:hostelite/home_screen_Student.dart';
 import 'package:image_picker/image_picker.dart';
 
-
-
 class StudentComplaint extends StatefulWidget {
   const StudentComplaint({Key key}) : super(key: key);
 
   @override
   _StudentComplaintState createState() => _StudentComplaintState();
-
 }
 
 firebase_storage.FirebaseStorage storage =
     firebase_storage.FirebaseStorage.instance;
 
-firebase_storage.Reference ref = storage.ref().child('images/defaultProfile.png');
+firebase_storage.Reference ref =
+    storage.ref().child('images/defaultProfile.png');
 
 Dialog leadDialog = Dialog(
   child: Container(
@@ -35,13 +33,15 @@ Dialog leadDialog = Dialog(
             width: 150,
           ),
         ),
-        Text('Entry Marked',
+        Text(
+          'Entry Marked',
           style: TextStyle(
             fontSize: 30,
             fontWeight: FontWeight.bold,
           ),
         ),
-        Text('Successfully',
+        Text(
+          'Successfully',
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 30,
@@ -51,22 +51,19 @@ Dialog leadDialog = Dialog(
           width: 150.0,
           height: 40.0,
           child: MaterialButton(
-            child: Text('Done',style: TextStyle(color: Colors.white,fontSize: 17.0),),
+            child: Text(
+              'Done',
+              style: TextStyle(color: Colors.white, fontSize: 17.0),
+            ),
             color: Color(0xffFE96FA),
             minWidth: 100.0,
-
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
             onPressed: () {
               BuildContext context;
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context){
-                        return HomeScreenStudent();
-                      }
-                  )
-              );
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return HomeScreenStudent();
+              }));
             },
           ),
         ),
@@ -76,21 +73,20 @@ Dialog leadDialog = Dialog(
 );
 
 class _StudentComplaintState extends State<StudentComplaint> {
-
-  var imageUrl='str';
+  var imageUrl = 'str';
 
   File img;
   getImage() async {
-     ImagePicker _picker = ImagePicker();
+    ImagePicker _picker = ImagePicker();
     final XFile image = await _picker.pickImage(source: ImageSource.camera);
-     if (image != null){
-       setState(() {
-         img = File(image.path);
-       });
-     }
+    if (image != null) {
+      setState(() {
+        img = File(image.path);
+      });
+    }
   }
 
-  Future uploadImageToFirebase(BuildContext context) async{
+  Future uploadImageToFirebase(BuildContext context) async {
     // String fileName = basename(img);
     // StorageReference firebaseStorageRef =
     // FirebaseStorage.instance.ref().child('uploads/$fileName');
@@ -100,24 +96,19 @@ class _StudentComplaintState extends State<StudentComplaint> {
     //         (value) => print("Done: $value"),
   }
 
-
-
-
   String roomNumber;
   String issue;
   String explanation;
-
-  // String url;
-  // String downloadURL;
-  //
-  // firebase_storage.FirebaseStorage storage = firebase_storage.FirebaseStorage.instance;
-  //
-  // firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
-  //     .ref()
-  //     .child('images')
-  //     .child('$img');
-
-
+  String status = "pending";
+  int counter = 0;
+  Map<String, dynamic> allComplaints = {
+    "issue": '',
+    "roomNumber": '',
+    "explanation": '',
+    "userUid": FirebaseAuth.instance.currentUser.uid,
+    "imageUrl": '',
+    "status": "Pending"
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -131,42 +122,30 @@ class _StudentComplaintState extends State<StudentComplaint> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-
-            Container(
-
-              child: TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Room No.',
-                filled: true,
-                fillColor: Color(0xffF7F7F7),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.0),
-                  borderSide: BorderSide(
-                      color: Colors.grey,
-                      width: 1.0
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        color: Colors.cyan,
-                        width: 1.0
-                    )
-                ),
-              ),
-                onChanged: (value) {
-                  setState(() {
-                    roomNumber = value.trim();
-                  });
-                }
-
-            ),
-          ),
-
-
-              SizedBox(height: 20,),
-
               Container(
-
+                child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Room No.',
+                      filled: true,
+                      fillColor: Color(0xffF7F7F7),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.cyan, width: 1.0)),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        roomNumber = value.trim();
+                      });
+                    }),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
                 child: TextFormField(
                     decoration: InputDecoration(
                       hintText: 'Your issue',
@@ -174,155 +153,130 @@ class _StudentComplaintState extends State<StudentComplaint> {
                       fillColor: Color(0xffF7F7F7),
                       enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(12.0),
-                        borderSide: BorderSide(
-                            color: Colors.grey,
-                            width: 1.0
-                        ),
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
                       ),
                       focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Colors.cyan,
-                              width: 1.0
-                          )
-                      ),
+                          borderSide:
+                              BorderSide(color: Colors.cyan, width: 1.0)),
                     ),
                     onChanged: (value) {
                       setState(() {
                         issue = value.trim();
                       });
-                    }
-
-                ),
+                    }),
               ),
-
-              SizedBox(height: 20,),
-
+              SizedBox(
+                height: 20,
+              ),
               TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(
-                  hintText: 'Type an explanation',
-                  filled: true,
-                  fillColor: Color(0xffF7F7F7),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                    borderSide: BorderSide(
-                        color: Colors.grey,
-                        width: 1.0
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    hintText: 'Type an explanation',
+                    filled: true,
+                    fillColor: Color(0xffF7F7F7),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(color: Colors.grey, width: 1.0),
                     ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.cyan, width: 1.0)),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.cyan,
-                          width: 1.0
-                      )
-                  ),
-                ),
                   onChanged: (value) {
                     setState(() {
-                      explanation  = value.trim();
+                      explanation = value.trim();
                     });
-                  }
+                  }),
+              SizedBox(
+                height: 20,
               ),
-
-              SizedBox(height: 20,),
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  MaterialButton(onPressed:(){
-                    var image = getImage();
-                    print(image);
-
-
-                  },
-
-
-                  child:  Text('Upload Image',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Color(0xff1C48E7)
-                  ),),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      side: BorderSide(color: Color(0xff1C48E7))
+                  MaterialButton(
+                    onPressed: () {
+                      var image = getImage();
+                      print(image);
+                    },
+                    child: Text(
+                      'Upload Image',
+                      style: TextStyle(fontSize: 16, color: Color(0xff1C48E7)),
                     ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(color: Color(0xff1C48E7))),
                   ),
                 ],
               ),
-
-              SizedBox(height: 20,),
-
-              Container(
-                child: (img!=null) ? Image.file(img) : Text(''),
+              SizedBox(
+                height: 20,
               ),
-
+              Container(
+                child: (img != null) ? Image.file(img) : Text(''),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  MaterialButton(onPressed: () async {
+                  MaterialButton(
+                    onPressed: () async {
+                      firebase_storage.UploadTask uploadedImg =
+                          ref.putFile(img);
+                      await uploadedImg.whenComplete(() => null);
 
+                      String url = "";
 
-                    firebase_storage.UploadTask uploadedImg = ref.putFile(img);
-                    await uploadedImg.whenComplete(() => null);
+                      await ref.getDownloadURL().then((value) {
+                        url = value;
+                      });
 
-                    String url = "";
+                      allComplaints = {
+                        "issue": issue,
+                        "roomNumber": roomNumber,
+                        "explanation": explanation,
+                        "userUid": FirebaseAuth.instance.currentUser.uid,
+                        "imageUrl": url,
+                        "status": status
+                      };
 
-                    await ref.getDownloadURL().then((value) {
-                    url = value;
-                    });
+                      await FirebaseFirestore.instance
+                          .collection("studentUsers")
+                          .doc(FirebaseAuth.instance.currentUser.uid)
+                          .collection("complaints")
+                          .doc(FirebaseAuth.instance.currentUser.uid)
+                          .set({
+                        "complain $counter": allComplaints,
 
+                        // "issue": issue,
+                        // "roomNumber": roomNumber,
+                        // "explanation": explanation,
+                        // "userUid": FirebaseAuth.instance.currentUser.uid,
+                        // "imageUrl": url,
+                        // "status": "Pending"
+                      });
 
-                    // var receivedUrl = await uploadedImg;
+                      counter++;
 
-                      // if (ref.)
-                   await FirebaseFirestore.instance.
-                    collection("studentUsers").
-                    doc(FirebaseAuth.instance.currentUser.uid).
-                    collection("complaints").
-                    doc(FirebaseAuth.instance.currentUser.uid).
-                    set({
-                      // "hostelName" : hostelName,
-                      "issue" : issue,
-                      "roomNumber" : roomNumber,
-                      "explanation" : explanation,
-                      "userUid" : FirebaseAuth.instance.currentUser.uid,
-                      "imageUrl" : url,
-                     "status":"Pending"
-                    });
-
-
-
-                    // showDialog(
-                        // context: context,
-                        // ignore: non_constant_identifier_names
-                        // builder: (BuildContext) => leadDialog
-                    // );
-                  },
+                      // showDialog(
+                      // context: context,
+                      // ignore: non_constant_identifier_names
+                      // builder: (BuildContext) => leadDialog
+                      // );
+                    },
                     color: Color(0xffFE96FA),
                     elevation: 10,
-                  child: Text('Send',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Color(0xff33004A)
-                  ),),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      side: BorderSide(color: Color(0xff1C48E7))
-                    ),
-                  ),
-
-                  Container(
                     child: Text(
-                      imageUrl
+                      'Send',
+                      style: TextStyle(fontSize: 16, color: Color(0xff33004A)),
                     ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(color: Color(0xff1C48E7))),
+                  ),
+                  Container(
+                    child: Text(imageUrl),
                   )
                 ],
               )
-
-
-
-
             ],
           ),
         ),
@@ -330,5 +284,3 @@ class _StudentComplaintState extends State<StudentComplaint> {
     );
   }
 }
-
-
