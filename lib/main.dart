28f22679a591f1932ec94e08862384f0d029344a
RitwikSingh28 <itsritwik26@gmail.com>
2022-07-17 11:32:58 +0530

    Improve the Landing Page UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hostelite/admin_screens/createAccountAdmin.dart';
+import 'package:hostelite/starting_pages/onboarding.dart';
 import 'package:hostelite/student_screens/createAccountStudent.dart';
 import 'package:hostelite/student_screens/exit_entryStudent.dart';
 import 'package:hostelite/admin_screens/home_screen_Admin.dart';
@@ -8,9 +9,6 @@ import 'package:hostelite/student_screens/home_screen_Student.dart';
 import 'package:hostelite/admin_screens/loginAdmin.dart';
 import 'package:hostelite/student_screens/loginStudent.dart';
 import 'package:hostelite/student_screens/mark_exit.dart';
-import 'package:hostelite/starting_pages/fast_and_easy.dart';
-import 'package:hostelite/starting_pages/keep_track.dart';
-import 'package:hostelite/starting_pages/solve_your_issues.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
@@ -25,9 +23,7 @@ Future<void> main() async {
     debugShowCheckedModeBanner: false,
     routes: {
       // '/': (context) => HomeScreenStudent(),
-      '/solveyourissues': (context) => SolveYourIssues(),
-      '/fastandeasy': (context) => FastAndEasy(),
-      '/keeptrack': (context) => KeepTrack(),
+      '/boardingpage': (context) => BoardingPage(),
       '/createaccountstudent': (context) => CreateAccountStudent(),
       '/createaccountadmin': (context) => CreateAccountAdmin(),
       '/loginstudent': (context) => LoginStudent(),
@@ -64,6 +60,6 @@ class InitialScreenDecider extends StatelessWidget {
     //     });
     return (FirebaseAuth.instance.currentUser != null)
         ? HomeScreenStudent()
-        : SolveYourIssues();
+        : BoardingPage();
   }
 }
